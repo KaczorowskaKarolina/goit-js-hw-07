bc1e897063709b5ccd0a98ec678b8c8d08673103
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-08-04 15:05:20 +0200

    task 1 gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+function renderGallery() {
+  const galleryElement = document.querySelector('.gallery');
+  const galleryHTML = galleryItems.map((item) => `
+    <li class="gallery__item">
+      <a href="#" class="gallery__link">
+        <img src="${item.preview}" alt="${item.alt}" class="gallery__image" data-source="${item.original}">
+      </a>
+    </li>
+  `).join('');
+  galleryElement.innerHTML = galleryHTML;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderGallery();
+});
+
+const galleryElement = document.querySelector('.gallery');
+galleryElement.addEventListener('click', (event) => {
+  if (event.target.classList.contains('gallery__image')) {
+    event.preventDefault();
+    const largeImageURL = event.target.dataset.source;
+    const lightboxImage = document.querySelector('#lightbox');
+    lightboxImage.src = largeImageURL;
+    const lightbox = BasicLightbox.create(lightboxImage.outerHTML);
+    lightbox.show();
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  if (event.keyCode === 27) {
+    const lightbox = document.querySelector('#lightbox');
+    lightbox.close();
+  }
+});
