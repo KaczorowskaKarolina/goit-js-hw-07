6a0487f308c04451512cddf56033c9a97d7070be
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-08-09 20:03:28 +0200

    new code for task 2 and esc for task 1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+function renderGallery() { const galleryElement = document.querySelector('.gallery'); const galleryHTML = galleryItems.map((item) =>
+    <li class="galleryitem">
+      <a class="gallerylink" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+    </li>
+).join(''); galleryElement.innerHTML = galleryHTML; }
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderGallery();
+  const lightbox = new SimpleLightbox('.gallerylink', {
+    captionsData: 'description',
+    captionPosition: 'bottom',
+    captionDelay: 250
+  });
+});
 console.log(galleryItems);
