32308ed6f99c21ce4dfa6fcad6af30a4fd0b1b14
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-08-12 23:02:48 +0200

    new codes version

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,34 +1,69 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
 
-function renderGallery() {
-  const galleryElement = document.querySelector('.gallery');
-  const galleryHTML = galleryItems.map((item) => `
-    <li class="gallery__item">
-      <a href="#" class="gallery__link">
-        <img src="${item.preview}" alt="${item.description}" class="gallery__image" data-source="${item.original}">
-      </a>
-    </li>
-  `).join('');
-  galleryElement.innerHTML = galleryHTML;
+const gallery = document.querySelector(".gallery");
+let lightbox;
+
+const divItem = ({ preview, original, description }) => `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+`;
+
+const getImages = (img) => img.map((item) => divItem(item)).join("");
+
+gallery.innerHTML = getImages(galleryItems);
+
+gallery.addEventListener("click", onClickGallery);
+
+const genBigImg = ({ url, description }) => `
+  <img
+    class="gallery__image"
+    src="${url}"
+    width="1280"
+    alt="${description}"/>
+`;
+
+function onClickGallery(e) {
+  e.preventDefault();
+
+  const url = e.target.dataset.source;
+  const description = e.target.alt;
+
+  if (!(url && description)) return;
+
+  lightbox = basicLightbox.create(genBigImg({ url, description }), {
+    onClose: () => {
+      gallery.removeEventListener("keydown", onKeyDownEsc);
+    },
+  });
+  lightbox.show();
+  gallery.addEventListener("keydown", onKeyDownEsc);
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  renderGallery();
-});
-
-const galleryElement = document.querySelector('.gallery');
-galleryElement.addEventListener('click', (event) => {
-  if (event.target.classList.contains('gallery__image')) {
-    event.preventDefault();
-    const largeImageURL = event.target.dataset.source;
-    const lightboxImage = document.querySelector('#lightbox');
-    lightboxImage.src = largeImageURL;
-    const lightbox = basicLightbox.create( `<img src="${largeImageURL}" alt="${event.target.description}">`);
-    lightbox.show();
-    window.addEventListener('keydown', (event) => {
-      if (event.key === 'Escape') {
-        basicLightbox.close();
-    }
-  });
+const onKeyDownEsc = (e) => {
+  if (e.key === "Escape" || e.keyCode === 27) {
+    lightbox.close();
   }
-});
+  console.log(e.key);
+};
+
+// -------------------------------------------------
+
+// Importuje się tablicę galleryItems z pliku gallery-items.js.
+// Ustala się zmienną gallery, która odnosi się do elementu o klasie "gallery".
+// Ustala się zmienną lightbox.
+// Tworzona jest funkcja divItem, która przyjmuje obiekt z właściwościami preview, original i description. Tworzy ona kod HTML dla pojedynczego elementu galerii i zwraca go.
+// Tworzona jest funkcja getImages, która przyjmuje tablicę obiektów i za pomocą funkcji map i join tworzy kod HTML dla wszystkich elementów galerii i zwraca go.
+// Przypisuje się kod HTML dla wszystkich elementów galerii do wewnętrznego HTML elementu gallery za pomocą gallery.innerHTML.
+// Dodaje się nasłuchiwanie na kliknięcie w galerii za pomocą gallery.addEventListener i funkcji onClickGallery.
+// Tworzona jest funkcja genBigImg, która przyjmuje obiekt z właściwościami url i description. Tworzy ona kod HTML dla dużego obrazu (lightbox) i zwraca go.
+// Tworzona jest funkcja onClickGallery, która przyjmuje zdarzenie e. Zatrzymuje ona domyślne działanie zdarzenia. Pobiera adres URL i opis obrazu z atrybutów dataset i alt klikniętego elementu. Jeśli któryś z atrybutów jest pusty, zwraca early return. Tworzony jest lightbox za pomocą funkcji basicLightbox.create, która przyjmuje kod HTML dużego obrazu i obiekt z funkcją onClose, która jest wywoływana przy zamykaniu lightboxa. Wyświetla się lightbox za pomocą lightbox.show(). Dodaje się nasłuchiwanie na naciśnięcie klawisza "Escape" za pomocą addEventListener i funkcji onKeyDownEsc.
+// Tworzona jest funkcja onKeyDownEsc, która przyjmuje zdarzenie e. Jeśli kliknięty klawisz to "Escape" lub jego odwzorowanie to 27, zamyka się lightbox za pomocą lightbox.close().
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,20 +1,41 @@
-import SimpleLightbox from 'simplelightbox';
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
-function renderGallery() { const galleryElement = document.querySelector('.gallery'); const galleryHTML = galleryItems.map((item) =>
-    <li class="galleryitem">
-      <a class="gallerylink" href="${item.original}">
-        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-      </a>
-    </li>
-).join(''); galleryElement.innerHTML = galleryHTML; }
 
-window.addEventListener('DOMContentLoaded', () => {
-  renderGallery();
-  const lightbox = new SimpleLightbox('.gallerylink', {
-    captionsData: 'description',
-    captionPosition: 'bottom',
-    captionDelay: 250
-  });
+const gallery = document.querySelector(".gallery");
+
+const liItem = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+  </li>
+`;
+
+const getImages = (img) => img.map((item) => liItem(item)).join("");
+
+gallery.innerHTML = getImages(galleryItems);
+
+gallery.addEventListener("click", onClickGallery);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
 });
+
+function onClickGallery(e) {
+  e.preventDefault();
+}
+
 console.log(galleryItems);
+
+// ----------------------------------------------
+
+// Importuje obiekt galleryItems z pliku "./gallery-items.js".
+// Pobiera element HTML o klasie "gallery" i przypisuje go do zmiennej gallery.
+// Definiuje funkcję liItem, która jako argument przyjmuje obiekt zawierający pola preview, original i description. Funkcja ta zwraca kod HTML reprezentujący pojedynczy element listy (<li>) w galerii, zawierający link do oryginalnego obrazka i podglądową miniaturkę (<img>).
+// Definiuje funkcję getImages, która jako argument przyjmuje tablicę obiektów img. Funkcja ta mapuje każdy obiekt item na kod HTML reprezentujący pojedynczy element listy przy użyciu funkcji liItem, a następnie łączy te kody w jedną wielką ciągłą linię za pomocą metody join("").
+// Ustawia kod HTML wygenerowany przez funkcję getImages(galleryItems) jako zawartość elementu o klasie "gallery" przy użyciu właściwości innerHTML.
+// Dodaje nasłuchiwanie na zdarzenie "click" na elemencie o klasie "gallery" i wywołuje funkcję onClickGallery w odpowiedzi na to zdarzenie.
+// Inicjalizuje bibliotekę SimpleLightbox, która umożliwia wyświetlanie obrazków w większym rozmiarze po kliknięciu na ich miniaturkę w galerii. Ta biblioteka używa selektora ".gallery a" do znalezienia odpowiednich linków w galerii i konfiguruje, żeby opisy obrazków były pobierane z atrybutu "alt" elementów <img>, a opóźnienie na wyświetlanie podpisów wynosi 250ms.
+// Definiuje funkcję onClickGallery, która zatrzymuje domyślną akcję po kliknięciu na element z klasą "gallery" (w tym przypadku linki z obrazkami).
+// Wyświetla w konsoli zawartość tablicy galleryItems.
